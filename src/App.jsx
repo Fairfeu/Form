e8bc8cdb8ba5eefb2566a14db31b981643142e96
Oleg <fnytseu@gmail.com>
2025-05-14 21:51:58 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import RegistrationForm from '../Components/RegistrationForm'
+function App() {
+
+  return (
+    <>
+      <RegistrationForm />
+    </>
+  )
+}
+
+export default App
